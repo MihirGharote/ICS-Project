bcaaca41c0be0b8418509afb7d201bc2df6bf08b
samarth <b25me1068@iitj.ac.in>
2026-03-13 19:10:32 +0530

    Defined struct in main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+typedef struct game {
+    char name[50];
+} something;
 
 int main() {
     printf("The game runs!\n");
+    something e;
+    strcpy(e.name, "PLEASE!!");
+    printf("%s", e.name);
+    return 0;
 }
