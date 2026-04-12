be8df6ae456ff5ff798e350927b228e8e492ed18
Priyalpatel16 <priyal1608patel@gmail.com>
2026-04-12 10:34:41 +0530

    Refactor reaction test output and formatting

--- Reactiontest.c
+++ Reactiontest.c
@@ -2,46 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
 void manual_delay(int seconds) {
     clock_t start_time = clock();
-   
     while (clock() < start_time + seconds * CLOCKS_PER_SEC);
 }
-
 int main() {
-    printf(" REACTION TEST (Standard C)\n");
-    printf("Press ENTER to start...");
+    printf(" REACTION TEST\n");
+    printf("Press ENTER to start");
     getchar();
-
     srand(time(NULL));
-   
     int delay_seconds = rand() % 4 + 2;
-
     printf("\nWaiting...");
     fflush(stdout);
-
-   
     manual_delay(delay_seconds);
-
-    printf("\n--- NOW! ---");
+    printf("\n NOW!");
     fflush(stdout);
-
     clock_t start_reaction = clock();
-    
-   
     while (getchar() != '\n'); 
-    
     clock_t end_reaction = clock();
-
     double total_time = (double)(end_reaction - start_reaction) / CLOCKS_PER_SEC;
-
     if (total_time < 0.05) {
-        printf("\nToo fast! You likely anticipated the prompt.\n");
+        printf("\nToo fast!\n");
     } else {
         printf("\nReaction Time: %.3f seconds\n", total_time);
     }
-
-    return 0;
+ return 0;
 }
