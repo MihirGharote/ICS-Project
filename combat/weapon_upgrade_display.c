32d70a9f2230e9ac9cc6694f4dc569bcab2160f1
rudri Bosamia <b25cs1060@iitj.ac.in>
2026-04-15 18:06:25 +0530

    new file added to display the weapon transformation and the result file added to story 2 for goblin and gryphon , options changed

---
+++ combat/weapon_upgrade_display.c
@@ -0,0 +1,162 @@
+#if defined(__has_include)
+#  if __has_include(<ncurses.h>)
+#    include <ncurses.h>
+#  elif __has_include(<curses.h>)
+#    include <curses.h>
+#  else
+#    include <stdio.h>
+#    define printw printf
+#    define refresh() ((void)0)
+#    define napms(x) ((void)0)
+#  endif
+#else
+#  include <ncurses.h>
+#endif
+
+#include <string.h>
+#include "./items.h"
+#include "weapon_upgrade_display.h"
+
+
+#define TOTAL_WEAPON_SLOTS 6
+
+
+static void dramatic_text(const char *text, int delay)
+{
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        printw("%c", text[i]);
+        refresh();
+        napms(delay);
+    }
+
+    printw("\n");
+    refresh();
+}
+
+
+static void show_weapon_upgrade(const char *old_name,
+                                const char *new_name)
+{
+    printw("\n");
+
+    dramatic_text("Your wisdom increases...", 20);
+
+    napms(250);
+
+    dramatic_text("Your weapon responds to your growth...", 25);
+
+    napms(350);
+
+    printw("\n");
+
+    dramatic_text("Previous weapon:", 15);
+    dramatic_text(old_name, 40);
+
+    napms(250);
+
+    printw("\n");
+
+    dramatic_text("New weapon unlocked:", 15);
+    dramatic_text(new_name, 40);
+
+    napms(350);
+
+    printw("\n");
+
+    dramatic_text("You feel its power flow through you.", 25);
+
+    printw("\n");
+
+    refresh();
+}
+
+
+
+static void show_shield_upgrade(const char *old_name,
+                                const char *new_name)
+{
+    printw("\n");
+
+    dramatic_text("Your defense strengthens...", 25);
+
+    napms(300);
+
+    dramatic_text("Your shield transforms...", 25);
+
+    napms(350);
+
+    printw("\n");
+
+    dramatic_text("Previous shield:", 15);
+    dramatic_text(old_name, 40);
+
+    napms(250);
+
+    printw("\n");
+
+    dramatic_text("New shield unlocked:", 15);
+    dramatic_text(new_name, 40);
+
+    napms(350);
+
+    printw("\n");
+
+    dramatic_text("Your protection has reached a new level.", 25);
+
+    printw("\n");
+
+    refresh();
+}
+
+
+void check_weapon_evolution(int slot_id,
+                            int wisdom_level,
+                            Weapon *current_weapon)
+{
+    Weapon evolved_weapon;
+
+    evolved_weapon = get_evolved_weapon(slot_id,
+                                        wisdom_level);
+
+    if (strcmp(current_weapon->name,
+                evolved_weapon.name) != 0)
+    {
+        show_weapon_upgrade(current_weapon->name,
+                            evolved_weapon.name);
+
+        *current_weapon = evolved_weapon;
+    }
+}
+
+
+
+void check_all_weapon_upgrades(int wisdom_level,
+                                Weapon weapon_slots[])
+{
+    for (int i = 0; i < TOTAL_WEAPON_SLOTS; i++)
+    {
+        check_weapon_evolution(i,
+                                wisdom_level,
+                                &weapon_slots[i]);
+    }
+}
+
+
+
+void check_shield_upgrade(int wisdom_level,
+                            Armor *current_shield)
+{
+    Armor evolved_shield;
+
+    evolved_shield = get_evolved_shield(wisdom_level);
+
+    if (strcmp(current_shield->name,
+                evolved_shield.name) != 0)
+    {
+        show_shield_upgrade(current_shield->name,
+                            evolved_shield.name);
+
+        *current_shield = evolved_shield;
+    }
+}
